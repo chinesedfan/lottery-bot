48adacaccfaaca2fe5bfc828aa5c672f9c0aaac8
Xianming Zhong <chinesedfan@qq.com>
2020-06-09 20:39:15 +0800

    no prepare job and create issue

--- src/actions/buy/index.js
+++ src/actions/buy/index.js
@@ -1,17 +1,38 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 const lotteries = require('../../lotteries')
 
 try {
+  const ghToken = core.getInput('ghToken')
+  const octokit = github.getOctokit(ghToken)
   const today = (new Date()).getDay()
 
   lotteries.forEach(({ name, redeemDay, buy }) => {
     if (today === redeemDay - 1) {
       const actual = buy()
-      core.info(`Bought ${name}: ${actual}`)
+      saveLotteryResult(octokit, name, actual)
+        .catch(error) => {
+          core.setFailed(error.message)
+        }
     } else {
       core.info(`No ${name} toady`)
     }
   })  
 } catch (error) {
   core.setFailed(error.message)
+}
+
+async function saveLotteryResult(octokit, name, actual) {
+  const newIssue = await octokit.issues.create({
+    owner: 'chinesedfan',
+    repo: 'lottery-bot',
+    title: `[${name}] ${actual}`,
+    labels: [] // TODO:
+  })
+
+  if (newIssue) {
+    core.info(`Bought ${name}: ${actual}`)
+  } else {
+    core.info('Failed to save lottery result as issue')
+  }
 }
